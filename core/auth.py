8206c29b6bbce5e28e8f5e5e566ae4bae2df1d7b
Arthur Gabriel <arthurgabriel73@gmail.com>
2022-06-24 00:15:11 -0300

    Working on core

---
+++ core/auth.py
@@ -0,0 +1,66 @@
+from pytz import timezone
+
+from datetime import datetime, timedelta
+
+from fastapi.security import OAuth2PasswordBearer
+
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from jose import jwt
+
+from models.user_model import UserModel
+from core.configs import settings
+from core.security import verify_password
+from pydantic import EmailStr
+
+
+oauth2_schema = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/users/login"
+)
+
+
+async def authenticate(email: EmailStr,
+                       password: str,
+                       db: AsyncSession
+                       ) -> UserModel: #Optional
+    async with db as session:
+        query = select(UserModel).filter(UserModel.email == email)
+        result = await session.execute(query)
+        user: UserModel = result.scalars().unique().one_or_none()
+
+        if not user:
+            return None
+
+        if not verify_password(password, user.password):
+            return None
+
+        return user
+
+
+def _create_token(type_token: str, time_life: timedelta, sub: str
+                 ) -> str:
+    # https://datatracker.ietf.org/doc/html/rfc7519#section-4.1.3
+    payload = {}
+    mg = timezone('America/Minas_Gerais')
+    expire = datetime.now(tz=mg) + time_life
+
+    payload["type"] = type_token
+
+    payload["exp"] = expire
+
+    payload["iat"] = datetime.now(tz=mg)
+
+    payload["sub"] = str(sub)
+
+    return jwt.encode(payload, settings.JWT_SECRET,
+                      algorithm=settings.ALGORITHM)
+
+
+def create_token_access(sub: str) -> str:
+
+    return _create_token(
+        type_token='access_token',
+        time_life=timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES),
+        sub=sub
+    )
